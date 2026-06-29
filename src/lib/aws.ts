73d1a68ff3d6a6f37ff4df253118026a98073eec
Rasul.dev <rasulbvc@gmail.com>
2026-06-29 16:18:51 +0530

    Create aws.ts

---
+++ src/lib/aws.ts
@@ -0,0 +1,16 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({
+  region: process.env.AWS_REGION || "us-east-1",
+  credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID || "",
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY || "",
+  },
+});
+
+export const dynamoDocClient = DynamoDBDocumentClient.from(client, {
+  marshallOptions: { removeUndefinedValues: true },
+});
+
+export const TABLE_NAME = process.env.DYNAMODB_TABLE_NAME || "LogPulse_Master_Store";
